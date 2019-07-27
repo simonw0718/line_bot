b5b2daf71c3f25533c6e76af021c0ce3957e7779
Simon Wang <simonw0718@gmail.com>
2019-07-27 23:08:13 +0800

    add

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def get_redirect_url():
                    _external = True,  #external = trun才是public的
                    _scheme = 'https') #因為可以產生http or https
 #上面fun會回傳oauth callback 路徑
-
+#11
 
 @app.route("/login")
 def login():
